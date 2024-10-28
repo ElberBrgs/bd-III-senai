3981493763e221483a55f636f01bcc03f28538f5
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-10-28 14:22:25 +0000

    To do: concluir menu.

--- app/main.py
+++ app/main.py
@@ -9,18 +9,35 @@ def main():
     service = UsuarioService(repository)
 
     #Solicitando dados para o usuário.
-    print("\nAdicionando usuário.")
-    nome = input("Digite seu nome: ")
-    email = input("Digite seu email: ")
-    senha = input("Digite sua senha: ")
 
-    service.criar_usuario(nome=nome,email=email,senha=senha)
+    while True:
+        print("\n///Código/// \t ///Descrição///")
+        print("1 \t Adicionar usuário.")
+        print("2 \t Pesquisar um usuário.")
+        print("3 \t Exibir usuários.")
+    
+        
+        resposta = int(input("Informe o código desejado: "))
+        system("cls || clear")
+        
+        match(resposta):
+            case 1:
+                print("\nAdicionando usuário.")
+                nome = input("Digite seu nome: ")
+                email = input("Digite seu email: ")
+                senha = input("Digite sua senha: ")
 
-    #Listar todos os usuários cadastrados.
-    print("\nListando todos os usuários cadastrados.")
-    lista_usuarios = service.listar_todos_usuarios()
-    for usuario in lista_usuarios:
-        print(f"Nome: {usuario.nome} - E-mail: {usuario.email} - Senha: {usuario.senha}")
+                service.criar_usuario(nome=nome,email=email,senha=senha)
+            case 2:
+                print("\nListando todos os usuários cadastrados.")
+                lista_usuarios = service.listar_todos_usuarios()
+                for usuario in lista_usuarios:
+                    print(f"Nome: {usuario.nome} - E-mail: {usuario.email} - Senha: {usuario.senha}")
+                break
+            case _:
+                print("Código inválido, tente novamente.")            
+
+
 
 if __name__ == "__main__":
     system("cls || clear")
